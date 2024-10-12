05d801bc17b1d3878ba1c187569a0d6d69230aa3
Herbert Joseph <techniquea2z@gmail.com>
2024-10-12 18:31:35 -0400

    feat(auth): Allow users to login with email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,21 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   validates :avatar, content_type: /\Aimage\/.*\z/
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+
+    if (login = conditions.delete(:login))
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
