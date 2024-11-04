3f442f78d5555adbce2b40782c2cd5786e58ad67
Herbert Joseph <techniquea2z@gmail.com>
2024-11-04 09:28:20 -0500

    fix(before-action): skip before_action for Hello#index

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,5 @@
 class HelloController < ApplicationController
+  skip_before_action :authenticate!
   def index
     render json: { success: true, message: "Welcome to Mystery Babylon Webtoon API" }
   end
